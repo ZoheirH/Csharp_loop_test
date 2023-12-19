9df4c5fbe21a8e4692975ccc5c4ee0f435901e62
Z H <zh13832020@gmail.com>
2023-12-19 19:02:32 +0330

    first commit

---
+++ loop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            for (int i = 0; i <a; )
+            {
+                
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("You are in the loop :)");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("------------------");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write("Dont Worry");
+
+
+
+            }
+
+        }
+    }
+}
